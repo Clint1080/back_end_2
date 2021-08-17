26b3ccfbd9b520d6d37a6d796c1274ddc5b93093
Clint Edwards <cedwards1080@gmail.com>
2021-08-17 14:52:32 -0600

    Finished

---
+++ server/controller.js
@@ -0,0 +1,48 @@
+const houses = require('./db.json')
+
+
+let globalId = 4
+
+
+module.exports = {
+    getAllHouses: (req, res) => {
+        res.status(200).send(houses)
+    },
+
+    createHouse: (req, res) => {
+        let { address, price, imageURL } = req.body
+        let newHouse = {
+          id: houses[houses.length - 1].id + 1,
+          address,
+          price,
+          imageURL
+        }
+        houses.push(newHouse)
+        res.status(200).send(houses)
+        // globalId++
+    },
+
+    updateHouse: (req, res) => {
+        let {id} = req.params
+        let {type} = req.body
+        let index = houses.findIndex(elem => +elem.id === +id)
+
+        if (houses[index].price === 0 && type === "minus") {
+          res.status(400).send(`You don't get paid to buy a house`);
+        } else if (type === "plus") {
+            houses[index].price+=10000
+            res.status(200).send(houses)
+        } else if (type === "minus" ) {
+            houses[index].price-=10000
+            res.status(200).send(houses)
+        } else {
+          res.status(400).send("Something went wrong");
+        }
+    },
+
+    deleteHouse: (req, res) => {
+        let index = houses.findIndex(elem => +elem.id === +req.params.id)
+        houses.splice(index, 1)
+        res.status(200).send(houses)
+    }
+}
--- server/index.js
+++ server/index.js
@@ -6,10 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const ctrl = require("./controller.js");
 
-
-
-
+app.get('/api/houses', ctrl.getAllHouses)
+app.post('/api/houses', ctrl.createHouse)
+app.put('/api/houses/:id', ctrl.updateHouse)
+app.delete('/api/houses/:id', ctrl.deleteHouse)
 
 
 
